81ba5a2ee9ee14ca09c7d5a6a0b9bf9c941c6df2
Lucas Oeming <bassclarinet1969@gmail.com>
2022-12-04 14:00:50 -0500

    gfgfgf

--- usacoPython/usacoPython.py
+++ usacoPython/usacoPython.py
@@ -1,41 +1,19 @@
 #  NAME                        :   Lucas Oeming
 #  GROUP                       :   APCS
 #  LAST MODIFICATION DATE      :   12/2/2022
-#  PROBLEM ID                  :   USACO - Bessie Come Home
-#  PROBLEM DESCRIPTION         :   Find the shortest possible path from a node to any
-#                              :   other 'filled' node.
+#  PROBLEM ID                  :   USACO - Cow Tours
+#  PROBLEM DESCRIPTION         :   Find the best way to connect two graphs in order to keep
+#                              :   the graph diamater to a minimum
 #  PEOPLE WHO I HELPED         :   N/A
 #  PEOPLE WHO HELPED ME        :   N/A
 
 '''
 ID: basscla1
 LANG: PYTHON3 
-TASK: comehome
+TASK: cowtour
 '''
 
-from json.encoder import INFINITY
-
-def dist(one, two):
-    return ((one[0]-two[0])**2 + (one[1]-two[1])**2)**0.5
-
-def connected(matrix):
-    for i in matrix:
-        if True in i:
-            start = i.index(True)
-            break
-    visited = set(())
-    visited.add(start)
-    while True: 
-        l = len(visited)
-        if l == len(matrix): 
-            return True
-        for k in visited.copy(): 
-            indexes = [i for i, j in enumerate(matrix[k]) if j == True]
-            visited.update(indexes) 
-        if l == len(visited):
-            return False
-
-
+from math import inf, dist
 
 
 # python equivalent of java nextInt function, returns next integer in input file when called
@@ -63,36 +41,62 @@ def main():
             else:
                 temp.append(True) 
         matrix.append(temp)
+        
+    pasone = set(())
+    for i in matrix:
+        if True in i:
+            pasone.add(i.index(True))
+            break
+    else:
+        solution = min([dist(vertices[p], vertices[p+1]) for p in range(N-1)])
+        outfile = open("cowtour.out", "w") 
+        outfile.write("{:.6f}".format(round(solution, 6)) + "\n")
+        outfile.close()
+        exit()
+    while True: 
+        l = len(pasone)
+        for k in pasone.copy(): 
+            indexes = [i for i, j in enumerate(matrix[k]) if j == True]
+            pasone.update(indexes) 
+        if l == len(pasone):
+            break
+    pastwo = set(()) 
+    for i in range(N):
+        if i not in pasone:
+            pastwo.add(i)
+
+    distances = [[inf]*N for p in range(N)]
+    for I in range(N):
+        for J in range(N):
+            if I == J:
+                distances[I][J] = 0
+                continue 
+            elif not matrix[I][J]: continue
+            distances[I][J] = dist(vertices[I], vertices[J])
+    for K in range(N): 
+        for I in range(N): 
+            if I == K: continue
+            for J in range(N): 
+                if I == J or J == K:
+                    continue
+                if distances[I][K] + distances[K][J] < distances[I][J]:
+                    distances[I][J] = distances[I][K] + distances[K][J]
+    for I in range(N):
+        for J in range(N):
+            if distances[I][J] == inf:
+                distances[I][J] = 0
 
     solutions = []
-    for i in range(N): 
-        for j in range(i, N):
-            if matrix[i][j] == True or i == j: continue
-            matrix[i][j] = True 
-            matrix[j][i] = True
-            if connected(matrix):
-                distances = [[INFINITY]*N for p in range(N)]
-                for I in range(N):
-                    for J in range(N):
-                        if I == J and not matrix[I][J]: continue
-                        distances[I][J] = dist(vertices[I], vertices[J])
-                for K in range(N): 
-                    for I in range(N): 
-                        for J in range(N): 
-                            if distances[I][K] + distances[K][J] < distances[I][J]:
-                                distances[I][J] = distances[I][K] + distances[K][J]
-                max = 0
-                for I in range(N): 
-                    for J in distances[I]:
-                        if J > max: max = J
-                solutions.append(max)
+    for i in pastwo: 
+        for j in pasone:
+            if matrix[i][j]: continue
+            #print(str(i) + "  " + str(j) + "  " + str(max(distances[i])) + "  " + str(dist(vertices[i], vertices[j])) + "  " + str(max(distances[j])) + " =   " + str(max(distances[i]) + dist(vertices[i], vertices[j]) + max(distances[j])))
+            solutions.append(max(distances[i]) + dist(vertices[i], vertices[j]) + max(distances[j]))
 
-            matrix[i][j] = False
-            matrix[j][i] = False
 
     # output solution and close
     outfile = open("cowtour.out", "w") 
-    outfile.write(str(min(solutions))  + "\n")
+    outfile.write("{:.6f}".format(round(min(solutions), 6)) + "\n")
     outfile.close()
     exit()
 main()
